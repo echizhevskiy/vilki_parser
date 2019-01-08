e6031af4ffe8a0c255a8b88424cf814fa2467bc6
Yahor Chyzheuski <egorkachic@gmail.com>
2019-01-08 15:56:23 +0300

    Date have been parsed

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -3,32 +3,17 @@ class ParserController < ApplicationController
     require 'nokogiri'
 
     def parse_url
-        
         html = open('https://www.parimatch.by/sport/khokkejj/kkhl')
         doc = Nokogiri::HTML(html.read)
         doc.encoding = 'utf-8'
         
-        dates = []
-        doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el|
-           el.css('tbody.row1, tbody.row2, tbody.processed').each do |date| 
-                dates.push(test: date.css('tr.bk td')[1].text)
-           end        
-        #   dates.each_with_index { |item, i| dates.delete_at(i+1)}
-        #   dates.each {|get_date| puts get_date.delete('/:')}
+        dates_from_webpage = [] # 08/0115:30
+        dates_with_format = [] # 08011530 = 8 января 15.30
+        doc.css('div#z_container div#z_contentw div#oddsList div.container div.wrapper table#g11234133').each do |el|
+            el.css('tbody.row2, tbody.row1, tbody.processed').each {|date| dates_from_webpage.push(date.css('tr td')[1].text)} 
         end
-        puts dates
-
-        #tbody.row1 tr.bk td
-        #doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el| 
-        #    el.css('tbody.row1, tbody.processed').each do |date|
-        #        puts date.css('tr.bk td')[1]
-        #    end
-           # events.push(
-           #     date: dates,
-           #     event: event
-           # )
-        #end
+        dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
+        dates_from_webpage.each {|get_date| dates_with_format.push(get_date.delete('/:'))}
     end
-
 
 end
