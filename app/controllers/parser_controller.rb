e3b9d9f357649fca76a4303ea15a9543590b4c2e
Yahor Chyzheuski <Chyzheuski_Y@IBA>
2019-01-08 17:59:37 +0300

    Parsing teams from parimatch have been added

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -8,12 +8,17 @@ class ParserController < ApplicationController
         doc.encoding = 'utf-8'
         
         dates_from_webpage = [] # 08/0115:30
-        dates_with_format = [] # 08011530 = 8 января 15.30
+        dates_with_format = [] # 08011530 = 8 января 15.30 UTC +2
         doc.css('div#z_container div#z_contentw div#oddsList div.container div.wrapper table#g11234133').each do |el|
-            el.css('tbody.row2, tbody.row1, tbody.processed').each {|date| dates_from_webpage.push(date.css('tr td')[1].text)} 
+            el.css('tbody.row2, tbody.row1, tbody.processed').each do |date| 
+                dates_from_webpage.push(date_variable: date.css('tr td')[1].text.delete('/:'), 
+                                        event: date.css('tr td a.om').children.to_s.gsub('<br>', ' -- '),
+                                        total: date.css('tr td')[5].children.to_s
+                )
+            end
         end
-        dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
-        dates_from_webpage.each {|get_date| dates_with_format.push(get_date.delete('/:'))}
+       # dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
+        puts dates_from_webpage
     end
 
 end
