a1e098cc8258610e7d1c9cb37420c88c5c12c644
Yahor Chyzheuski <Chyzheuski_Y@IBA>
2019-01-09 14:52:29 +0300

    Wrote parser for teams, dates, type of match

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -2,20 +2,21 @@ class ParserController < ApplicationController
     require 'open-uri'
     require 'nokogiri'
 
+    #parser for parimatch
     def parse_url
         html = open('https://www.parimatch.by/sport/khokkejj/kkhl')
         doc = Nokogiri::HTML(html.read)
         doc.encoding = 'utf-8'
         
         dates_from_webpage = [] # 08/0115:30
-        dates_with_format = [] # 08011530 = 8 января 15.30 UTC +2
-        doc.css('div#z_container div#z_contentw div#oddsList div.container div.wrapper table#g11234133').each do |el|
+        doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el|
             el.css('tbody.row2, tbody.row1, tbody.processed').each do |date| 
                 dates_from_webpage.push(date_variable: date.css('tr td')[1].text.delete('/:'), 
-                                        event: date.css('tr td a.om').children.to_s.gsub('<br>', ' -- '),
-                                        total: date.css('tr td')[5].children.to_s
+                                        match: date.css('tr td a.om').children.to_s.gsub('<br>', ' -- '),
+                                        type: doc.css('div#z_container div#z_contentw div#oddsList div.container h3').text
                 )
             end
+            dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
         end
        # dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
         puts dates_from_webpage
