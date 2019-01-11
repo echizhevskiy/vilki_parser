1ff765b01814d1c715d33b7588cf3bbbc0ec7122
Yahor Chyzheuski <Chyzheuski_Y@IBA>
2019-01-11 17:04:44 +0300

    try to parse totals

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -10,33 +10,56 @@ class ParserController < ApplicationController
         doc.encoding = 'utf-8'
 
         #binding.pry
-       # doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el|
-           # el.xpath("//*[contains(@class, 'row1') and contains(@class, 'props')]").each do |date|
-           #     puts date.text
-           # end
-           # el.css('tbody.row1').each do |date|
-           #     puts date.text
-           # end
-       # end
+        # <tbody class="row1"></tbody>
+        # <tbody class="row1 props"></tbody>
 
-        dates_from_webpage = [] # 08/01 15:30
         doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el|
-            el.css('tbody.row2, tbody.row1').each do |date| 
-                dates_from_webpage.push(date_variable: date.css('tr td')[1].text.insert(5, ' '), 
-                                        match: date.css('tr td a.om').children.to_s.gsub('<br>', ' -- '), 
+            el.css('tbody.row1.props').each do |data|
+                data['class']="row1 props processed"
+            end
+            el.css('tbody.row2.props').each do |data|
+                data['class']="row2 props processed"
+            end
+        end
+
+        dates_from_webpage = []
+        total_data = []
+        doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |tbody|
+            tbody.css('tbody.row1', 'tbody.row2').each do |data|
+                dates_from_webpage.push(date_variable: data.css('tr td')[1].text.insert(5, ' '), 
+                                        match: data.css('tr td a.om').children.to_s.gsub('<br>', ' -- '), 
                                         type: doc.css('div#z_container div#z_contentw div#oddsList div.container h3').text
-                )
+                                       )
             end
             dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
 
-            el.css('tbody.row1.props tr table.ps tbody tr').each do |setting|
-                puts setting
+            #binding.pry
+            tbody.css('tbody.row1.props.processed tr td table.ps').each do |td|
+                total_data.push(total: td.css('tr td')[0].text) 
+            end
+        end
+        puts total_data
+        #puts dates_from_webpage
+
+
+        #dates_from_webpage = [] # 08/01 15:30
+        #doc.css('div#z_container div#z_contentw div#oddsList table.dt').each do |el|
+        #    el.css('tbody.row2, tbody.row1').each do |date| 
+        #        dates_from_webpage.push(date_variable: date.css('tr td')[1].text.insert(5, ' '), 
+        #                                match: date.css('tr td a.om').children.to_s.gsub('<br>', ' -- '), 
+        #                                type: doc.css('div#z_container div#z_contentw div#oddsList div.container h3').text
+        #        )
+        #    end
+        #    dates_from_webpage.each_with_index { |item, i| dates_from_webpage.delete_at(i+1)}
+
+           # el.css('tbody.row1.props tr table.ps tbody tr').each do |setting|
+           #     setting.css('')
                 #dates_from_webpage.push(total: setting.css('tr')[12].text
                                         #attr_1: setting.css('tr td ')
                 #                         )
-            end
-        end
-        puts dates_from_webpage
+           # end
+        #end
+        #puts dates_from_webpage
     end
 
 end
