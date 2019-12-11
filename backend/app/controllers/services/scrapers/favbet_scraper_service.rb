4066350c7759482cba93e18eb024bd290a714d58
echizhevskiy <egorkachi@mail.ru>
2019-12-11 19:12:14 +0300

    Added ability to click on buttons to each event in leagua

--- backend/app/controllers/services/scrapers/favbet_scraper_service.rb
+++ backend/app/controllers/services/scrapers/favbet_scraper_service.rb
@@ -2,36 +2,42 @@ module Services
     module Scrapers
         class FavbetScraperService < BaseScraperService
             def parse(link, match_kind)
-                Headless.ly do
+              #  Headless.ly do
                     binding.pry
                     browser = Watir::Browser.new
                         start_parsing_time = Time.now
 
                         browser.goto(link)
+=begin
+                        doc = Nokogiri::HTML(browser.html)
+                        doc.encoding = 'utf-8'
 
-                        browser.is(class: /event--head-block/).each do |event|
+                        doc.css('ul.events--list').each do |event|
                             binding.pry
-                            event.is(class: /event--more/).button.click
+                            puts "hello, man"
                         end
-                        
-                        browser.is(class: /event--more/).each do |button|
+=end
+                        browser.lis(class: /event--head-block/).each do |event|
                             binding.pry
-                            button.is(button).click
-                            sleep(0.4)
-        
+
                             doc = Nokogiri::HTML(browser.html)
                             doc.encoding = 'utf-8'
-                                            
-                            binding.pry
-            
-                            doc.css('div.wrapper full--version--site div.contentdiv div#middle div#container div.prebet div.column2').each do |data|
-                                date = data.search('div:nth-child(1)').search('div:nth-child(2)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(4)').search('div:nth-child(1)').text #.search('div:nth-child(4)').search('div:nth-child(1)').search('ul:nth-child(1)')
+                            event.divs(class: /event--more/).each do |k|
+                                k.button.click
 
+                                doc.css('div.wrapper full--version--site div.contentdiv div#middle div#container div.prebet div.column2').each do |data|
+                                    #date = data.search('div:nth-child(1)').search('div:nth-child(2)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(1)').search('div:nth-child(4)').search('div:nth-child(1)').text #.search('div:nth-child(4)').search('div:nth-child(1)').search('ul:nth-child(1)')
+                                    
+
+                                    puts "hi vasya"
+                                end
                             end
+                            sleep(0.4)
                         end
+
                     browser.close
                 end
-            end
+           # end
         end
     end
 end
